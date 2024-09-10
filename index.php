8c758798a0e8e80d63d0f8fe3858f54c26382223
Abibou Wade <91915110+biboucodeur@users.noreply.github.com>
2024-09-10 21:04:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion - Sunu-Plateforme</title>
+    <title>Connexion</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <style>
