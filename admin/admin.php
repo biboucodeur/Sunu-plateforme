9be79dc3641e13652bb29000e4ab65f82392fb35
Abibou Wade <91915110+biboucodeur@users.noreply.github.com>
2024-10-03 16:15:50 +0000

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -34,7 +34,7 @@ $bdd = null;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tableau de bord - Admin</title>
+    <title>Tableau de bord</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <style>
