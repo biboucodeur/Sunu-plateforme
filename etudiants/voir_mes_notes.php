8a624f869bdb9226f43aab10e2055570d8138492
Abibou Wade <91915110+biboucodeur@users.noreply.github.com>
2024-10-06 22:25:39 +0000

    Update voir_mes_notes.php

--- etudiants/voir_mes_notes.php
+++ etudiants/voir_mes_notes.php
@@ -23,7 +23,7 @@ $notes = $stmt_notes_etudiant->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mes Notes</title>
+    <title>Mes Notes - Sunu-plateforme</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .jumbotron {
