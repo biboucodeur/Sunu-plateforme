2fb954dc80b3a5555ff8103973076491163ea825
Abibou Wade <91915110+biboucodeur@users.noreply.github.com>
2025-06-18 10:29:36 +0000

    Update inscription.php

--- etudiants/inscription.php
+++ etudiants/inscription.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($resultat) {
             $succes = "Inscription réussie. Vous pouvez maintenant vous connecter.";
         } else {
-            $erreur = "Une erreur est survenue lors de l'inscription. Veuillez réessayer.";
+            $erreur = "Une erreur est survenue lors de l'inscription. Veuillez réessayer !";
         }
     }
 }
